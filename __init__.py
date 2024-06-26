92802499f66018adfac3aa16b90778e3dd153925
Yuri DaBang <158317180+Yuri-DaBang@users.noreply.github.com>
2024-06-26 19:20:29 +0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# Infiniti project's goal is to give programmers the sense of liberation that comes
-# from the beauty of the code itself, even if it hurts productivity!
-# We understand that beauty is not objective...
-
-__author__ = 'Yrui-DaBang'
-__license__ = 'MIT'
-__version__ = 'iota => 3.1'
